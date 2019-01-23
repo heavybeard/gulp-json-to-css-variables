1db3a3406379362294c50a9cc6c0bf2663cd457a
Andrea Cognini <andrea.cognini@docebo.com>
2019-01-23 10:53:41 +0100

    feat: create package

---
+++ index.js
@@ -0,0 +1,20 @@
+const jsonToCssVariables = require('json-to-css-variables');
+
+/**
+ * Transform JSON to CSS
+ */
+module.exports = () => {
+    const Transform = require('stream').Transform;
+    let transformStream = new Transform({ objectMode: true });
+
+    transformStream._transform = (file, encoding, callback) => {
+        file.path = `${file.path.substr(0, file.path.lastIndexOf('.json'))}.css`;
+        file.contents = Buffer.from(
+            jsonToCssVariables(JSON.parse(file.contents.toString()), { unit: '', pretty: true }),
+            encoding,
+        );
+        callback(null, file);
+    };
+
+    return transformStream;
+};
